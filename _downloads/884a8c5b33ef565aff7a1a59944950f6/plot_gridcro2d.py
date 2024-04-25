7b939a64b4d0670697806998af67a4b7b2358c13
barronh <barronh@users.noreply.github.com>
2024-04-25 17:00:54 +0000

    deploy: 01ded8ceb41abdbe2ea252fad54ebd1f7b0e81b5

---
+++ _downloads/884a8c5b33ef565aff7a1a59944950f6/plot_gridcro2d.py
@@ -0,0 +1,15 @@
+"""
+GRIDCRO2D Terrain Height Plot
+=============================
+
+This example shows how to create a plot of terrain heigh from one day of EPA MCIP."""
+
+
+# !python -m pip install epaaws
+import epaaws
+
+f = epaaws.mp2022.open_gridcro2d('2022-01-01')
+levels = [-100, -10, 1, 100, 200, 400, 800, 1600, 3200]
+qm = f['HT'].plot(levels=levels, cmap='terrain')
+f.csp.cno.drawstates()
+qm.figure.savefig('terrain.png')
